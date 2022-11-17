f6c5838f994d60f11625ed86d9c99d32da5570a8
tantality <angel.tretyak@mail.ru>
2022-11-17 23:55:48 +0300

    feat: add task entity

---
+++ src/resources/tasks/task.entity.ts
@@ -0,0 +1,21 @@
+import { TASK_STATUS, TASK_TYPE } from './tasks.constants';
+
+let tasksCounter = 0;
+
+export class Task {
+  id: number;
+
+  constructor(
+    public readonly userId: number,
+    public readonly hiddenWordId: number,
+    public readonly type: TASK_TYPE,
+    public readonly status: TASK_STATUS,
+    public readonly correctAnswers: string[] | null,
+    public readonly receivedAnswer: string | null,
+    public readonly createdAt: Date,
+    public readonly updatedAt: Date,
+  ) {
+    this.id = tasksCounter;
+    tasksCounter += 1;
+  }
+}
