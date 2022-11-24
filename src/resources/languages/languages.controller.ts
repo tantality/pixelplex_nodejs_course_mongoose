825f871dc093d415d83ce1dfcca84b2d25c1c1ad
tantality <angel.tretyak@mail.ru>
2022-11-24 23:31:49 +0300

    feat: specify the return value for the controller methods according to the documentation

--- src/resources/languages/languages.controller.ts
+++ src/resources/languages/languages.controller.ts
@@ -1,41 +1,37 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable require-await */
-import { NextFunction, Request, Response } from 'express';
-import { logRequest } from '../../utils/log-request.utils';
+import { NextFunction, Response } from 'express';
+import {
+  CreateLanguageRequest,
+  DeleteLanguageRequest,
+  GetLanguagesRequest,
+  GetOneLanguageRequest,
+  UpdateLanguageRequest,
+} from './types/request.types';
+import { LanguagesService } from './languages.service';
 
 export class LanguagesController {
-  static getLanguages = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    logRequest(req);
-    res.status(200).json({
-      message: 'The operation was successful',
-    });
+  static getLanguages = async (req: GetLanguagesRequest, res: Response, next: NextFunction): Promise<void> => {
+    const languages = await LanguagesService.findAll(req);
+    res.status(200).json(languages);
   };
 
-  static getOneLanguage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    logRequest(req);
-    res.status(200).json({
-      message: 'The operation was successful',
-    });
+  static getOneLanguage = async (req: GetOneLanguageRequest, res: Response, next: NextFunction): Promise<void> => {
+    const language = await LanguagesService.findById(req);
+    res.status(200).json(language);
   };
 
-  static createLanguage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    logRequest(req);
-    res.status(200).json({
-      message: 'The operation was successful',
-    });
+  static createLanguage = async (req: CreateLanguageRequest, res: Response, next: NextFunction): Promise<void> => {
+    const createdLanguage = await LanguagesService.create(req);
+    res.status(201).json(createdLanguage);
   };
 
-  static updateLanguage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    logRequest(req);
-    res.status(200).json({
-      message: 'The operation was successful',
-    });
+  static updateLanguage = async (req: UpdateLanguageRequest, res: Response, next: NextFunction): Promise<void> => {
+    const updatedLanguage = await LanguagesService.update(req);
+    res.status(200).json(updatedLanguage);
   };
 
-  static deleteLanguage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    logRequest(req);
-    res.status(200).json({
-      message: 'The operation was successful',
-    });
+  static deleteLanguage = async (req: DeleteLanguageRequest, res: Response, next: NextFunction): Promise<void> => {
+    const languageId = await LanguagesService.delete(req);
+    res.status(200).json({ id: languageId });
   };
 }
