615da2f56a393ead2035fd87383d80d46e367e6a
tantality <angel.tretyak@mail.ru>
2022-11-24 23:06:24 +0300

    feat: type the request object for the languages module

---
+++ src/resources/languages/types/index.ts
@@ -0,0 +1,4 @@
+export * from './request.types';
+export * from './params.types';
+export * from './body.types';
+export * from './query.types';
---
+++ src/resources/languages/types/query.types.ts
@@ -0,0 +1,9 @@
+interface IWithBaseQuery {
+  search: string;
+  offset: number;
+  limit: number;
+  sortDirection: string;
+  sortBy: string;
+}
+
+export type GetLanguagesQuery = IWithBaseQuery;
