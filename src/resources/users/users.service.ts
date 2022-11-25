4838c735977ee44879a682660d0050e68b0e744c
tantality <angel.tretyak@mail.ru>
2022-11-25 20:31:33 +0300

    feat: add service methods for the users module

--- src/resources/users/users.service.ts
+++ src/resources/users/users.service.ts
@@ -1,1 +1,31 @@
-export class UsersService {}
+/* eslint-disable require-await */
+import { Request } from 'express';
+import { logRequest } from '../../utils/log-request.utils';
+import { UpdateUserRequest } from './types';
+import { UserDTO } from './user.dto';
+import { User } from './user.entity';
+
+export class UsersService {
+  private static user = new User(
+    1,
+    'Angelina',
+    'email@gmail.com',
+    'email@gmail.com',
+    'qwerty123',
+    'user',
+    'awdwkmkwad243',
+    new Date(),
+    new Date(),
+  );
+  private static userDTO = new UserDTO(UsersService.user);
+
+  static findById = async (req: Request): Promise<UserDTO | null> => {
+    logRequest(req);
+    return UsersService.userDTO;
+  };
+
+  static update = async (req: UpdateUserRequest): Promise<UserDTO | null> => {
+    logRequest(req);
+    return UsersService.userDTO;
+  };
+}
