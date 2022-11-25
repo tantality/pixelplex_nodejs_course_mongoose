7ef787af16c1c390aa2e17552f3442d965eb982f
tantality <angel.tretyak@mail.ru>
2022-11-25 23:18:06 +0300

    feat: add service methods for the auth module

--- src/resources/auth/auth.service.ts
+++ src/resources/auth/auth.service.ts
@@ -1,1 +1,42 @@
-export class AuthService {}
+/* eslint-disable require-await */
+import { Request } from 'express';
+import { logRequest } from '../../utils/log-request.utils';
+import { User } from '../users/user.entity';
+import { AuthDTO } from './auth.dto';
+import { SignUpRequest, LogInRequest } from './types';
+
+export class AuthService {
+  private static user = new User(
+    1,
+    'Angelina',
+    'email@gmail.com',
+    'email@gmail.com',
+    'qwerty123',
+    'user',
+    'awdwkmkwad243',
+    new Date(),
+    new Date(),
+  );
+
+  private static authDTO = new AuthDTO(AuthService.user, 'amdwiwnf');
+
+  static signUp = async (req: SignUpRequest): Promise<AuthDTO> => {
+    logRequest(req);
+    return AuthService.authDTO;
+  };
+
+  static logIn = async (req: LogInRequest): Promise<AuthDTO> => {
+    logRequest(req);
+    return AuthService.authDTO;
+  };
+
+  static logOut = async (req: Request): Promise<number> => {
+    logRequest(req);
+    return 1;
+  };
+
+  static refresh = async (req: Request): Promise<AuthDTO> => {
+    logRequest(req);
+    return AuthService.authDTO;
+  };
+}
