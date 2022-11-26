6e78b14e6479b2dabb929594f07972c450390d66
tantality <angel.tretyak@mail.ru>
2022-11-26 03:42:38 +0300

    feat: apply types in the tasks module controller

--- src/resources/tasks/tasks.controller.ts
+++ src/resources/tasks/tasks.controller.ts
@@ -1,34 +1,38 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable require-await */
 import { NextFunction, Request, Response } from 'express';
-import { logRequest } from '../../utils/log-request.utils';
+import { TasksService } from './tasks.service';
+import {
+  GetTasksRequest,
+  GetTasksResponse,
+  GetStatisticsRequest,
+  GetStatisticsResponse,
+  CreateTaskRequest,
+  CreateTaskResponse,
+  AddAnswerToTaskRequest,
+  AddAnswerToTaskResponse,
+  GetTasksCommon,
+  GetStatisticsCommon,
+  CreateTaskCommon,
+} from './types';
 
 export class TasksController {
-  static getTasks = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    logRequest(req);
-    res.status(200).json({
-      message: 'The operation was successful',
-    });
+  static getTasks = async (req: GetTasksRequest, res: GetTasksResponse, next: NextFunction): Promise<void> => {
+    const tasks = await TasksService.findAll(req);
+    res.status(200).json(tasks as GetTasksCommon);
   };
 
-  static getStatistics = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    logRequest(req);
-    res.status(200).json({
-      message: 'The operation was successful',
-    });
+  static getStatistics = async (req: GetStatisticsRequest, res: GetStatisticsResponse, next: NextFunction): Promise<void> => {
+    const statistics = await TasksService.calculateStatistics(req);
+    res.status(200).json({ statistics: statistics as GetStatisticsCommon });
   };
 
-  static createTask = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    logRequest(req);
-    res.status(200).json({
-      message: 'The operation was successful',
-    });
+  static createTask = async (req: CreateTaskRequest, res: CreateTaskResponse, next: NextFunction): Promise<void> => {
+    const task = await TasksService.create(req);
+    res.status(200).json(task as CreateTaskCommon);
   };
 
-  static addAnswerToTask = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    logRequest(req);
-    res.status(200).json({
-      message: 'The operation was successful',
-    });
+  static addAnswerToTask = async (req: AddAnswerToTaskRequest, res: AddAnswerToTaskResponse, next: NextFunction): Promise<void> => {
+    const task = await TasksService.addAnswer(req);
+    res.status(200).json(task);
   };
 }
