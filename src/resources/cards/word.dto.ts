19d42bcec9ca44f9c1e12a4f33e9c35bdda5dd33
tantality <angel.tretyak@mail.ru>
2022-11-26 05:31:20 +0300

    feat: add dto for word

---
+++ src/resources/cards/word.dto.ts
@@ -0,0 +1,12 @@
+import { IWord } from './word.interface';
+
+type PartialWord = Pick<IWord, 'id' | 'value'>;
+
+export class WordDTO implements PartialWord {
+  public readonly id: number;
+  public readonly value: string;
+  constructor(word: IWord | PartialWord) {
+    this.id = word.id;
+    this.value = word.value;
+  }
+}
