279ab13866c43be32b5baa79263c115a98a912af
tantality <angel.tretyak@mail.ru>
2022-11-26 05:40:02 +0300

    feat: apply types in the cards module controller

--- src/resources/cards/cards.controller.ts
+++ src/resources/cards/cards.controller.ts
@@ -1,34 +1,37 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable require-await */
-import { NextFunction, Request, Response } from 'express';
-import { logRequest } from '../../utils/log-request.utils';
+import { NextFunction } from 'express';
+import { CardDTO } from './card.dto';
+import { CardsService } from './cards.service';
+import {
+  GetCardsRequest,
+  GetCardsResponse,
+  CreateCardRequest,
+  CreateCardResponse,
+  UpdateCardRequest,
+  UpdateCardResponse,
+  DeleteCardRequest,
+  DeleteCardResponse,
+  GetCardsCommon,
+} from './types';
 
 export class CardsController {
-  static getCards = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    logRequest(req);
-    res.status(200).json({
-      message: 'The operation was successful',
-    });
+  static getCards = async (req: GetCardsRequest, res: GetCardsResponse, next: NextFunction): Promise<void> => {
+    const cards = await CardsService.findAll(req);
+    res.status(200).json(cards as GetCardsCommon);
   };
 
-  static createCard = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    logRequest(req);
-    res.status(200).json({
-      message: 'The operation was successful',
-    });
+  static createCard = async (req: CreateCardRequest, res: CreateCardResponse, next: NextFunction): Promise<void> => {
+    const createdCard = await CardsService.create(req);
+    res.status(201).json(createdCard);
   };
 
-  static updateCard = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    logRequest(req);
-    res.status(200).json({
-      message: 'The operation was successful',
-    });
+  static updateCard = async (req: UpdateCardRequest, res: UpdateCardResponse, next: NextFunction): Promise<void> => {
+    const updatedCard = await CardsService.update(req);
+    res.status(200).json(updatedCard as CardDTO);
   };
 
-  static deleteCard = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    logRequest(req);
-    res.status(200).json({
-      message: 'The operation was successful',
-    });
+  static deleteCard = async (req: DeleteCardRequest, res: DeleteCardResponse, next: NextFunction): Promise<void> => {
+    const cardId = await CardsService.delete(req);
+    res.status(200).json({ id: cardId as number });
   };
 }
