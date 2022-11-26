1c324f24e855dd5835c362540b25c050b34417a0
tantality <angel.tretyak@mail.ru>
2022-11-26 05:56:46 +0300

    refactor: update routers

--- src/resources/cards/cards.router.ts
+++ src/resources/cards/cards.router.ts
@@ -3,13 +3,14 @@ import { checkSchema } from 'express-validator';
 import { validatePayload } from '../../middleware';
 import { CardsController } from './cards.controller';
 import { CardsValidation } from './cards.validation';
+import { CreateCardRequest, DeleteCardRequest, GetCardsRequest, UpdateCardRequest } from './types';
 
 const router = Router();
 
-router.get('/', checkSchema(CardsValidation.getCards), validatePayload, CardsController.getCards);
-router.post('/', checkSchema(CardsValidation.createCard), validatePayload, CardsController.createCard);
-router.patch('/:cardId', checkSchema(CardsValidation.updateCard), validatePayload, CardsController.updateCard);
-router.delete('/:cardId', checkSchema(CardsValidation.deleteCard), validatePayload, CardsController.deleteCard);
+router.get('/', checkSchema(CardsValidation.getCards), validatePayload<GetCardsRequest>, CardsController.getCards);
+router.post('/', checkSchema(CardsValidation.createCard), validatePayload<CreateCardRequest>, CardsController.createCard);
+router.patch('/:cardId', checkSchema(CardsValidation.updateCard), validatePayload<UpdateCardRequest>, CardsController.updateCard);
+router.delete('/:cardId', checkSchema(CardsValidation.deleteCard), validatePayload<DeleteCardRequest>, CardsController.deleteCard);
 
 export function mountCardsRouter(app: Application): void {
   app.use('/api/v1/cards', router);
--- src/resources/tasks/tasks.router.ts
+++ src/resources/tasks/tasks.router.ts
@@ -3,16 +3,17 @@ import { checkSchema } from 'express-validator';
 import { validatePayload } from '../../middleware';
 import { TasksController } from './tasks.controller';
 import { TasksValidation } from './tasks.validation';
+import { GetTasksRequest, GetStatisticsRequest, CreateTaskRequest, AddAnswerToTaskRequest } from './types';
 
 const router = Router();
 
-router.get('/', checkSchema(TasksValidation.getTasks), validatePayload, TasksController.getTasks);
-router.get('/statistics', checkSchema(TasksValidation.getStatistics), validatePayload, TasksController.getStatistics);
-router.post('/', checkSchema(TasksValidation.createTask), validatePayload, TasksController.createTask);
+router.get('/', checkSchema(TasksValidation.getTasks), validatePayload<GetTasksRequest>, TasksController.getTasks);
+router.get('/statistics', checkSchema(TasksValidation.getStatistics), validatePayload<GetStatisticsRequest>, TasksController.getStatistics);
+router.post('/', checkSchema(TasksValidation.createTask), validatePayload<CreateTaskRequest>, TasksController.createTask);
 router.post(
   '/:taskId/answer',
   checkSchema(TasksValidation.addAnswerToTask),
-  validatePayload,
+  validatePayload<AddAnswerToTaskRequest>,
   TasksController.addAnswerToTask,
 );
 
