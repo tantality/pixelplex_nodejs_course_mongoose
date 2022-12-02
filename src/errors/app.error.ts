c995652b559f62f208e680a388e36dd638b10885
tantality <angel.tretyak@mail.ru>
2022-12-02 19:13:43 +0300

    feat: add multiple error classes

--- src/errors/app.error.ts
+++ src/errors/app.error.ts
@@ -7,3 +7,27 @@ export class AppError extends Error {
 export function isAppError(error: unknown): error is AppError {
   return error instanceof AppError;
 }
+
+export class BadRequestError extends AppError {
+  constructor(message: string) {
+    super(message, 400);
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message: string) {
+    super(message, 401);
+  }
+}
+
+export class ForbiddenError extends AppError {
+  constructor(message: string = 'This action is available only to the administrator.') {
+    super(message, 403);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message: string) {
+    super(message, 404);
+  }
+}
