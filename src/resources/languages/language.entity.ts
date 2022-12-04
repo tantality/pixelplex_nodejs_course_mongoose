93863add0e7b5bf6b54a0b55a316f6c32d9d9255
tantality <angel.tretyak@mail.ru>
2022-12-05 01:02:34 +0300

    refactor: change formatting

--- src/resources/languages/language.entity.ts
+++ src/resources/languages/language.entity.ts
@@ -5,14 +5,18 @@ import { ILanguage } from './types';
 export class Language extends BaseEntity implements ILanguage {
   @PrimaryGeneratedColumn()
     id!: number;
+
   @Index()
   @Column({ type: 'varchar', length: 255 })
     name!: string;
+
   @Column({ type: 'varchar', length: 255, unique: true })
     code!: string;
+
   @Index()
   @CreateDateColumn()
     createdAt!: Date;
+
   @UpdateDateColumn()
     updatedAt!: Date;
 }
