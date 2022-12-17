5d6e6355d3ce521fc80f94a8571184c585f443e3
tantality <angel.tretyak@mail.ru>
2022-12-17 06:03:41 +0300

    refactor: use enums in a function

--- src/modules/languages/utils/get-sorting-condition.utils.ts
+++ src/modules/languages/utils/get-sorting-condition.utils.ts
@@ -1,14 +1,18 @@
 import { FindOptionsOrder } from 'typeorm';
+import { SORT_DIRECTION } from '../../../types';
 import { Language } from '../language.entity';
+import { LANGUAGE_SORT_BY } from '../types';
 
 export const getSortingCondition = (sortBy: string, sortDir: string): FindOptionsOrder<Language> => {
   let sortingCondition: FindOptionsOrder<Language> = {};
-  const sortDirection: 'ASC' | 'DESC' = sortDir.toUpperCase() as 'ASC' | 'DESC';
-  if (sortBy === 'name') {
+  const sortDirection: SORT_DIRECTION = sortDir as SORT_DIRECTION;
+
+  if (sortBy === LANGUAGE_SORT_BY.NAME) {
     sortingCondition = { name: sortDirection };
   }
-  if (sortBy === 'date') {
+  if (sortBy === LANGUAGE_SORT_BY.DATE) {
     sortingCondition = { createdAt: sortDirection };
   }
+
   return sortingCondition;
 };
