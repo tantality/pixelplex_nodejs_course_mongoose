4e9e40c77fedadc8df946f99980774bebbe87c92
tantality <angel.tretyak@mail.ru>
2022-12-18 22:37:47 +0300

    feat: implement a language model

---
+++ src/models/language.model.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import { ILanguage } from '../modules/languages/types';
+
+const languageSchema = new mongoose.Schema<ILanguage>(
+  {
+    name: { type: String, required: true },
+    code: { type: String, unique: true, index: true, required: true },
+  },
+  { timestamps: true },
+);
+
+languageSchema.index({ createdAt: 1 });
+
+export const Language = mongoose.model('language', languageSchema);
