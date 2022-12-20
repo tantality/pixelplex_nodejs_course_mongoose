525fdbdbf6f48b7fc0ba8913c8e90311c0655f63
tantality <angel.tretyak@mail.ru>
2022-12-20 22:13:13 +0300

    feat: implement a user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,17 @@
+import { model, Schema, Types } from 'mongoose';
+import { IUser, USER_ROLE } from '../modules/users/types';
+
+const userSchema = new Schema<IUser>(
+  {
+    nativeLanguageId: { ref: 'language', type: Types.ObjectId, default: null },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    normalizedEmail: { type: String, unique: true, index: true, required: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: [USER_ROLE.USER, USER_ROLE.ADMIN], default: USER_ROLE.USER },
+    refreshToken: { type: String, default: null },
+  },
+  { timestamps: true },
+);
+
+export const User = model('user', userSchema);
