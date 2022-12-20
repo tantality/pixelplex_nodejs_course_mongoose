2c84c0ba89709adca8eed1c118b71424e90d1d67
tantality <angel.tretyak@mail.ru>
2022-12-20 19:55:27 +0300

    feat: update dto

--- src/modules/languages/language.dto.ts
+++ src/modules/languages/language.dto.ts
@@ -1,13 +1,13 @@
 import { ObjectId } from 'mongoose';
 import { ILanguage } from './types';
 
-export class LanguageDTO implements Omit<ILanguage, 'updatedAt'> {
-  public readonly _id: ObjectId;
+export class LanguageDTO implements Omit<ILanguage, 'updatedAt' | '_id'> {
+  public readonly id: ObjectId;
   public readonly name: string;
   public readonly code: string;
   public readonly createdAt: Date;
   constructor(language: ILanguage) {
-    this._id = language._id;
+    this.id = language._id;
     this.name = language.name;
     this.code = language.code;
     this.createdAt = language.createdAt;
