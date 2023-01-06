5ac6b4335b332a75dd94de267ef15a766eed6aef
tantality <angel.tretyak@mail.ru>
2023-01-06 03:11:18 +0300

    refactor: move code to another file

--- src/modules/languages/languages.repository.ts
+++ src/modules/languages/languages.repository.ts
@@ -1,19 +1,42 @@
 import { FilterQuery, ObjectId, ProjectionType, QueryOptions } from 'mongoose';
 import { Language } from '../../models/language.model';
-import { SORT_DIRECTION } from '../../types';
-import { CreateLanguageBody, ILanguage, UpdateLanguageBody } from './types';
+import { CreateLanguageBody, GetLanguagesQuery, ILanguage, UpdateLanguageBody } from './types';
+import { getSortingCondition } from './utils';
 
 export class LanguagesRepository {
-  static findAndCountAll = async (
-    filter: FilterQuery<ILanguage>,
-    options: QueryOptions<ILanguage>,
-    sortingCondition: { [key: string]: SORT_DIRECTION },
-  ): Promise<{ count: number; languages: ILanguage[] }> => {
-    const selectionFields: ProjectionType<ILanguage> = { _id: 1, code: 1, name: 1, createdAt: 1 };
-    const languages = await Language.find(filter, selectionFields, options).sort(sortingCondition);
+  static findAndCountAll = async ({
+    search,
+    sortBy,
+    sortDirection,
+    limit,
+    offset,
+  }: GetLanguagesQuery): Promise<{ count: number; languages: ILanguage[] }> => {
+    const filter = LanguagesRepository.getFilter(search);
+
+    const options: QueryOptions<ILanguage> = {
+      skip: offset,
+      limit,
+    };
+
+    const sortingCondition = getSortingCondition(sortBy, sortDirection);
+    const fieldsToSelect: ProjectionType<ILanguage> = { _id: 1, code: 1, name: 1, createdAt: 1 };
+
+    const languages = await Language.find(filter, fieldsToSelect, options).sort(sortingCondition);
     const count = await LanguagesRepository.countAll(filter);
 
     return { count, languages };
+  };
+
+  private static getFilter(search?: string): FilterQuery<ILanguage> {
+    let filter: FilterQuery<ILanguage> = {};
+
+    if (search) {
+      filter = {
+        name: { $regex: new RegExp(search, 'i') },
+      };
+    }
+
+    return filter;
   };
 
   static countAll = async (condition: FilterQuery<ILanguage>): Promise<number> => {
