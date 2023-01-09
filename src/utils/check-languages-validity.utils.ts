84cab93b6587d8e6408e90f9a216763881ec73cb
tantality <angel.tretyak@mail.ru>
2023-01-10 02:23:40 +0300

    feat: add a function to check the validity of languages

---
+++ src/utils/check-languages-validity.utils.ts
@@ -0,0 +1,26 @@
+import { ObjectId } from 'mongoose';
+import {
+  BadRequestError,
+  NO_NATIVE_LANGUAGE_SET_FOR_THE_USER_MESSAGE,
+  NotFoundError,
+  LANGUAGE_NOT_FOUND_MESSAGE,
+  NATIVE_AND_FOREIGN_LANGUAGE_ARE_EQUAL_MESSAGE,
+} from '../errors';
+import { LanguagesService } from '../modules/languages/languages.service';
+
+export const checkLanguagesValidity = async (nativeLanguageId: ObjectId | null, foreignLanguageId?: ObjectId): Promise<void> => {
+  if (!nativeLanguageId) {
+    throw new BadRequestError(NO_NATIVE_LANGUAGE_SET_FOR_THE_USER_MESSAGE);
+  }
+
+  if (foreignLanguageId) {
+    if (foreignLanguageId.toString() === nativeLanguageId.toString()) {
+      throw new BadRequestError(NATIVE_AND_FOREIGN_LANGUAGE_ARE_EQUAL_MESSAGE);
+    }
+
+    const foreignLanguage = await LanguagesService.findOneByCondition({ _id: foreignLanguageId });
+    if (!foreignLanguage) {
+      throw new NotFoundError(LANGUAGE_NOT_FOUND_MESSAGE);
+    }
+  }
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './log-request.utils';
+export * from './check-languages-validity.utils';
