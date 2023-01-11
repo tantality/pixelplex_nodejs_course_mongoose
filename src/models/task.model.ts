aa239c236fbd4ca632de4d79985f0203042af1ce
tantality <angel.tretyak@mail.ru>
2023-01-11 23:37:48 +0300

    feat: implement a task model

---
+++ src/models/task.model.ts
@@ -0,0 +1,27 @@
+import { Schema, Types, model } from 'mongoose';
+import { TASK_TYPE, TASK_STATUS, ITask } from '../modules/tasks/types';
+
+const taskSchema = new Schema<ITask>(
+  {
+    userId: { ref: 'user', type: Types.ObjectId, index: true, required: true },
+    hiddenWord: { type: String, index: true, required: true },
+    nativeLanguageId: { ref: 'language', type: Types.ObjectId, required: true },
+    foreignLanguageId: { ref: 'language', type: Types.ObjectId, required: true },
+    type: { type: String, enum: Object.values(TASK_TYPE), required: true },
+    status: {
+      type: String,
+      enum: Object.values(TASK_STATUS),
+      default: TASK_STATUS.UNANSWERED,
+      index: true,
+      required: true,
+    },
+    correctAnswers: { type: [String], default: null },
+    receivedAnswer: { type: String, default: null },
+  },
+  { timestamps: true },
+);
+
+taskSchema.index({ createdAt: 1 });
+taskSchema.index({ nativeLanguageId: 1, foreignLanguageId: 1 });
+
+export const Task = model('task', taskSchema);
